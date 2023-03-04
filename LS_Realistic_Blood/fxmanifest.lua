b9f0d6eeacbb00b25f2a6d88073e098f57c042fe
Roberto <78977643+LS-Robert@users.noreply.github.com>
2023-03-04 03:06:19 +0100

    Add files via upload

---
+++ LS_Realistic_Blood/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "cerulean"
+game "gta5"
+
+author "Anto Orza#6115"
+description "Ls More Realistic Blood System"
+version "1.0.0"
+
+client_scripts { 
+	"config.lua",
+	"client/client.lua"
+} 
+
+server_script {
+	"config.lua",
+	"server/server.lua"
+}
